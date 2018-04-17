71e50de918c9065b9b06cbbf518628c2708930e4
jimmyitcode <383135252@qq.com>
2018-04-17 19:58:05 +0800

    wangzmssm

---
+++ wangzm-common/src/main/java/com/jimmy/common/pojo/echarts/XAxis.java
@@ -0,0 +1,15 @@
+package com.jimmy.common.pojo.echarts;
+
+public class XAxis {
+	
+	private String data[];
+
+	public String[] getData() {
+		return data;
+	}
+
+	public void setData(String[] data) {
+		this.data = data;
+	}
+
+}
